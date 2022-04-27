2f455068a95b734fff68a816ed4928760d03cd95
helenis33 <helenis33@gmail.com>
2022-04-27 07:33:59 +0200

    primera parte

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Tu código va aquí: 
+let edad = 16;
+
+
+if (edad < 17) {
+    console.log ("pueden entrar.")
+
+} else if (edad< 14) {
+    console.log ("lo siento, eres demasiado joven vuelve a intentarlo más adelante")
+
+
+
+} else { (edad <= 14 && edad <=17 ) 
+    console.log ("pueden entrar, pero sólo acompañadas de una persona mayor de 18 años")
+}
+
+
+/*Funciones
+Refactoriza el código anterior en una única función que se llame gestionarAdmision(). Ejecútala y comprueba el resultado de esta función con las
+siguientes edades: 0, 8, 12, 14, 17, 18, 35 y 99.*/
+
+gestionarAdmision ( 0,8,12,14,17,18,35,99 )
